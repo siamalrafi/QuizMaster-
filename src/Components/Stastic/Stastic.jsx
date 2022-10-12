43540eaeba70f5b2fe9c1c343b9e15cfd4d65f32
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-10-12 08:40:44 +0600

    This si the next

--- src/Components/Stastic/Stastic.jsx
+++ src/Components/Stastic/Stastic.jsx
@@ -7,14 +7,19 @@ const Stastic = () => {
     const data = useLoaderData().data;
     console.log(data);
     return (
-        <div className='mx-auto justify-start'>
-            <BarChart width={300} height={200} data={data}>
-                <XAxis dataKey="name" stroke="#8884d8" />
-                <YAxis />
-                <Tooltip />
-                <CartesianGrid stroke="#ccc" />
-                <Bar dataKey="total" fill="#8884d8" barSize={30} />
-            </BarChart>
+        <div className='w-full mx-auto  justify-items-center'>
+            <div className='mt-b bg-green-600 p-5 mb-5 flex justify-center font-bold text-2xl'>
+                <h1>Quiz Statistic</h1>
+            </div>
+            <div className='mt-16 flex justify-center'>
+                <BarChart width={400} height={300} data={data}>
+                    <XAxis dataKey="name" stroke="#8884d8" />
+                    <YAxis />
+                    <Tooltip />
+                    <CartesianGrid stroke="#ccc" />
+                    <Bar dataKey="total" fill="#8884d8" barSize={30} />
+                </BarChart>
+            </div>
         </div>
     );
 };
