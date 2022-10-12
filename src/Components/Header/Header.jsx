8ce9290b5a3af0178805565ed307459d7d51b194
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-10-12 09:03:37 +0600

    This is the part

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                     <div className="flex items-center justify-between">
                         <Link to="/" className=" text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-green-600" >
                             <FontAwesomeIcon icon={faQuestion} />
-                            <span to="/">QuizMaster </span>
+                            <span className='text-green-500' to="/">QuizMaster </span>
                         </Link>
                     </div>
                     <div className=" inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
